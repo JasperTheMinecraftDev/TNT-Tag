afffa32474c7c99b069096d4252bf428367ebef3
JasperTheDev <jurianpost@outlook.com>
2023-07-18 13:11:52 +0200

    Cleanup of the damage listener.

--- src/main/java/nl/juriantech/tnttag/listeners/EntityDamageByEntityListener.java
+++ src/main/java/nl/juriantech/tnttag/listeners/EntityDamageByEntityListener.java
@@ -4,13 +4,13 @@ import nl.juriantech.tnttag.Arena;
 import nl.juriantech.tnttag.Tnttag;
 import nl.juriantech.tnttag.enums.GameState;
 import nl.juriantech.tnttag.enums.PlayerType;
+import nl.juriantech.tnttag.managers.GameManager;
 import nl.juriantech.tnttag.utils.ChatUtils;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-
 
 public class EntityDamageByEntityListener implements Listener {
 
@@ -22,12 +22,9 @@ public class EntityDamageByEntityListener implements Listener {
 
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-        if (!(event.getEntity() instanceof Player) || !(event.getDamager() instanceof Player)) {
+        if (!(event.getEntity() instanceof Player victim) || !(event.getDamager() instanceof Player damager)) {
             return;
         }
-
-        Player damager = (Player) event.getDamager();
-        Player victim = (Player) event.getEntity();
 
         Arena damagerArena = plugin.getArenaManager().getPlayerArena(damager);
         Arena victimArena = plugin.getArenaManager().getPlayerArena(victim);
@@ -36,20 +33,24 @@ public class EntityDamageByEntityListener implements Listener {
             return;
         }
 
-        if (damagerArena.getGameManager().state == GameState.INGAME) {
-            if (damagerArena.getGameManager().playerManager.getPlayers().get(victim) == PlayerType.TAGGER) {
-                return; // Stop processing if the victim is a tagger
+        GameManager gameManager = damagerArena.getGameManager();
+        if (gameManager.state == GameState.INGAME) {
+            PlayerType victimType = gameManager.playerManager.getPlayerType(victim);
+            if (victimType == PlayerType.TAGGER) {
+                return;
             }
             victim.setHealth(28);
         } else {
             event.setCancelled(true);
         }
 
-        if (damagerArena.getGameManager().playerManager.getPlayerType(damager) == PlayerType.TAGGER
-                && damagerArena.getGameManager().playerManager.getPlayerType(victim) == PlayerType.SURVIVOR) {
+        PlayerType damagerType = gameManager.playerManager.getPlayerType(damager);
 
-            damagerArena.getGameManager().playerManager.setPlayerType(damager, PlayerType.SURVIVOR);
-            damagerArena.getGameManager().playerManager.setPlayerType(victim, PlayerType.TAGGER);
+        // Switch player types if the damager is a tagger and the victim is a survivor
+        if (damagerType == PlayerType.TAGGER) {
+            gameManager.playerManager.setPlayerType(damager, PlayerType.SURVIVOR);
+            gameManager.playerManager.setPlayerType(victim, PlayerType.TAGGER);
+
             ChatUtils.sendCustomMessage(victim, ChatUtils.getRaw("player.tagged").replace("{tagger}", damager.getName()));
 
             victim.playSound(victim.getLocation(), Sound.valueOf(ChatUtils.getRaw("sounds.tagged").toUpperCase()), 1, 1);
