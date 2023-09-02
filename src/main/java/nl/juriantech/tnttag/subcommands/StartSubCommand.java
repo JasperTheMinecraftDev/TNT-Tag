efdb7daca9841a74f8c926f48c0e7d13028eb104
JasperTheDev <jurianpost@outlook.com>
2023-09-02 13:35:41 +0200

    Added a forced option to the /tt start command.

--- src/main/java/nl/juriantech/tnttag/subcommands/StartSubCommand.java
+++ src/main/java/nl/juriantech/tnttag/subcommands/StartSubCommand.java
@@ -2,36 +2,39 @@ package nl.juriantech.tnttag.subcommands;
 
 import nl.juriantech.tnttag.Arena;
 import nl.juriantech.tnttag.Tnttag;
+import nl.juriantech.tnttag.enums.GameState;
 import nl.juriantech.tnttag.managers.ArenaManager;
 import nl.juriantech.tnttag.utils.ChatUtils;
 import org.bukkit.entity.Player;
 import revxrsal.commands.annotation.Command;
+import revxrsal.commands.annotation.Optional;
 import revxrsal.commands.annotation.Subcommand;
 import revxrsal.commands.bukkit.annotation.CommandPermission;
-
-import java.io.IOException;
 
 @Command({"tnttag", "tt"})
 public class StartSubCommand {
 
-    private final Tnttag plugin;
     private final ArenaManager arenaManager;
 
     public StartSubCommand(Tnttag plugin) {
-        this.plugin = plugin;
         this.arenaManager = plugin.getArenaManager();
     }
 
     @Subcommand("start")
     @CommandPermission("tnttag.start")
-    public void onStart(Player player, String arenaName) {
+    public void onStart(Player player, String arenaName, @Optional boolean forced) {
         Arena arena = arenaManager.getArena(arenaName);
         if (arena == null) {
             ChatUtils.sendMessage(player, "commands.invalid-arena");
             return;
         }
 
-        arena.getGameManager().start();
+        if (forced) {
+            arena.getGameManager().setGameState(GameState.INGAME);
+        } else {
+            arena.getGameManager().start();
+        }
+
         ChatUtils.sendMessage(player, "commands.arena-started");
     }
 }
