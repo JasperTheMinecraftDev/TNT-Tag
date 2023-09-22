894e6ff70879dae6586efab28d6ff86c86624c67
JasperTheDev <jurianpost@outlook.com>
2023-09-22 22:47:04 +0200

    Added confirmation messages to the dump commands.

--- src/main/java/nl/juriantech/tnttag/subcommands/DumpSubCommand.java
+++ src/main/java/nl/juriantech/tnttag/subcommands/DumpSubCommand.java
@@ -1,29 +1,72 @@
 package nl.juriantech.tnttag.subcommands;
 
 import nl.juriantech.tnttag.Tnttag;
+import nl.juriantech.tnttag.utils.ChatUtils;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.Subcommand;
 import revxrsal.commands.bukkit.annotation.CommandPermission;
 
-@Command({"tnttag", "tt"})
+import java.util.HashMap;
+import java.util.Map;
+
+@Command("tnttag|tt")
 public class DumpSubCommand {
 
     private final Tnttag plugin;
+    private final Map<Player, Long> firstExecutionTimes = new HashMap<>();
+    private final long executionTimeLimitMillis = 10000; // 10 seconds
 
     public DumpSubCommand(Tnttag plugin) {
         this.plugin = plugin;
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                long currentTimeMillis = System.currentTimeMillis();
+                firstExecutionTimes.entrySet().removeIf(entry -> (currentTimeMillis - entry.getValue()) > executionTimeLimitMillis);
+            }
+        }.runTaskTimer(plugin, 20L, 20L);
     }
 
     @Subcommand("dump all")
     @CommandPermission("tnttag.dump.all")
-    public void onDumpAll(CommandSender commandSender) {
-        plugin.getDumpManager().dumpAll(commandSender);
+    public void onDumpAll(Player player) {
+        if (hasExecutedTwice(player)) {
+            plugin.getDumpManager().dumpAll(player);
+            removeEntry(player);
+        } else {
+            addEntry(player);
+            ChatUtils.sendMessage(player, "&cWarning: This command should only be used if requested by the developer.");
+            ChatUtils.sendMessage(player, "&bExecute this command again within 10 seconds to proceed.");
+        }
     }
 
     @Subcommand("dump log")
     @CommandPermission("tnttag.dump.log")
-    public void onDumpLog(CommandSender commandSender) {
-        plugin.getDumpManager().dumpLog(commandSender);
+    public void onDumpLog(Player player) {
+        if (hasExecutedTwice(player)) {
+            plugin.getDumpManager().dumpLog(player);
+            removeEntry(player);
+        } else {
+            addEntry(player);
+            ChatUtils.sendMessage(player, "&cWarning: This command should only be used if requested by the developer.");
+            ChatUtils.sendMessage(player, "&bExecute this command again within 10 seconds to proceed.");
+        }
+    }
+
+    private boolean hasExecutedTwice(Player player) {
+        long currentTimeMillis = System.currentTimeMillis();
+        return firstExecutionTimes.containsKey(player) && (currentTimeMillis - firstExecutionTimes.get(player)) <= executionTimeLimitMillis;
+    }
+
+    private void addEntry(Player player) {
+        firstExecutionTimes.put(player, System.currentTimeMillis());
+    }
+
+    private void removeEntry(Player player) {
+        firstExecutionTimes.remove(player);
     }
 }
