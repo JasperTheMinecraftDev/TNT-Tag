a94789eda7e4062d039985acd7c1552e7d82693a
jurian.post <jurianpost@outlook.com>
2024-12-24 20:28:03 +0100

    Fix player XP not being properly restored.

--- src/main/java/nl/juriantech/tnttag/objects/PlayerInformation.java
+++ src/main/java/nl/juriantech/tnttag/objects/PlayerInformation.java
@@ -16,7 +16,8 @@ public class PlayerInformation {
     private final Location oldLocation;
     private final ItemStack[] inventory;
     private final ItemStack[] armor;
-    private final int totalExperience;
+    private final int level; // Store player's level
+    private final int totalExperience; // Store total experience points
     private final GameMode gameMode;
     private final int foodLevel;
     private final String displayName;
@@ -28,6 +29,7 @@ public class PlayerInformation {
         this.oldLocation = player.getLocation();
         this.inventory = player.getInventory().getContents();
         this.armor = player.getInventory().getArmorContents();
+        this.level = player.getLevel();
         this.totalExperience = player.getTotalExperience();
         this.gameMode = player.getGameMode();
         this.foodLevel = player.getFoodLevel();
@@ -54,7 +56,10 @@ public class PlayerInformation {
         player.getInventory().setContents(inventory);
         player.getInventory().setArmorContents(armor);
         player.setTotalExperience(totalExperience);
+        player.setLevel(level);
+
         if (!Tnttag.configfile.getBoolean("skip-location-restoral")) player.teleport(oldLocation);
+
         player.setGameMode(gameMode);
         player.setFoodLevel(foodLevel);
         player.setDisplayName(displayName);
